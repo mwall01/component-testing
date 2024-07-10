6dc548ec2a7dbce56c5a5d4e638f9b9bfec1ca7b
Michael Wall <48205225+mwall01@users.noreply.github.com>
2024-07-10 12:57:59 -0400

    Y

--- cypress/support/frameUtil.js
+++ cypress/support/frameUtil.js
@@ -1,7 +1,7 @@
 export const renderStorybookIframe = () => {
   cy.get('#storybook-preview-iframe').then(($iframe) => {
     cy.viewport(1200, 900);
-    cy.wait(5000);
+    cy.wait(2000);
     const iframe = $iframe.contents();
     cy.wrap(iframe).as('iframe');
   });
