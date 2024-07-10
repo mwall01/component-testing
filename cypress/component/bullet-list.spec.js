833a7103bc472f8f196e2eb6d4f56ef09be753d8
Louise Noone <45362037+louisenoone@users.noreply.github.com>
2024-07-10 09:52:56 -0400

    bullet list test

---
+++ cypress/component/bullet-list.spec.js
@@ -0,0 +1,21 @@
+const storyBookUtils = require('../support/frameUtil');
+
+
+describe('RTE bullet list test', () => { // e.g. bold
+    beforeEach(() => {
+      cy.visit('toolbar-options--bullet-list'); // e.g. toolbar-options--bold
+      storyBookUtils.renderStorybookIframe();
+    });
+  
+    // Create variables for your selectors
+    const textField = '.tiptap';
+    const bulletListButton = '[aria-label="Bullet List"]';
+    const bulletListText = '.tiptap p Bullet';
+  
+    // Update the below to test out your own component
+    it('unbullets content', () => {
+      cy.get('@iframe').find(textField).should('exist').click().type('{selectall}');
+      cy.get('@iframe').find(bulletListButton).should('exist').click();
+      cy.get('@iframe').find(bulletListText).should('not.exist');
+    });
+  });
